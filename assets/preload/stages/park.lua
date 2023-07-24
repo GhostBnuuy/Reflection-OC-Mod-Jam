b72dbfcb7750783ad9df9409f538fbfffc477637
Bunny <84008574+GhostBnuuy@users.noreply.github.com>
2023-07-24 14:26:49 +0200

    second stage added

--- assets/preload/stages/park.lua
+++ assets/preload/stages/park.lua
@@ -1,2 +1,27 @@
+local path = 'backgrounds/2/'
+
 function onCreate()
+    makeLuaSprite('sky', path..'nightsky', -418, -1200)
+    scaleObject('sky', 1.2, 1.2)
+    addLuaSprite('sky', false)
+
+    makeLuaSprite('bg', path..'bg', -410, -1180)
+    scaleObject('bg', 1.2, 1.2)
+    addLuaSprite('bg', false)
+
+    makeLuaSprite('bg2', path..'bg2', -370, -1200)
+    scaleObject('bg2', 1.2, 1.2)
+    addLuaSprite('bg2', false)
+
+    makeLuaSprite('fg', path..'fg', -400, 820)
+    scaleObject('fg', 1.2, 1.2)
+    addLuaSprite('fg', true)
+
+    makeLuaSprite('lighting', path..'lighting', -70, -400)
+    scaleObject('lighting', 1.2, 1.2)
+    addLuaSprite('lighting', true)
+end
+
+function onCreatePost()
+    setProperty('gf.visible', false)
 end
